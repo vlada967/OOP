339d6c05732b0852a243fa4afa9b7f227573758c
Vlada Arkhipova <v.arkhipova1@g.nsu.ru>
2021-11-07 19:17:34 +0600

    lab4_1

---
+++ Task_1_4_1/src/main/ru/nsu/fit/oop/task1_4_1/Atom.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.oop.task1_4_1;
+
+public enum Atom {
+    UNARY_OPERATOR,
+    BINARY_OPERATOR,
+    NUMBER,
+}
---
+++ Task_1_4_1/src/main/ru/nsu/fit/oop/task1_4_1/WrongArgumentsNumberException.java
@@ -0,0 +1,7 @@
+package ru.nsu.fit.oop.task1_4_1;
+
+public class WrongArgumentsNumberException extends CalculatorException{
+    public WrongArgumentsNumberException() {
+        super();
+    }
+}
