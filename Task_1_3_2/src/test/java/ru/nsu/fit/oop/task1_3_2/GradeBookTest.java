30751bb288248375dd2846147ee03adeb13853ac
Vlada Arkhipova <v.arkhipova1@g.nsu.ru>
2021-11-12 18:25:17 +0700

    lab3_2

--- Task_1_3_2/src/test/java/ru/nsu/fit/oop/task1_3_2/GradeBookTest.java
+++ Task_1_3_2/src/test/java/ru/nsu/fit/oop/task1_3_2/GradeBookTest.java
@@ -54,7 +54,7 @@ class GradeBookTest {
     @Test
     public void testHonorsDegree() {
         GradeBook book = new GradeBook(200708, "Vlada", "Arkhipova", "Computer science and system design", 3);
-        book.addAttestation(1, "Introduction to algebra and analysis", 4, "Vaskevich");
+        book.addAttestation(1, "Introduction to algebra and analysis", 5, "Vaskevich");
         book.addAttestation(1, "Introduction to discrete mathematics", 5, "Vlasov");
         book.addAttestation(1, "Declarative programming", 5, "Vlasov");
         book.addAttestation(1, "Imperative programming", 5, "Nesterenko");
@@ -62,7 +62,7 @@ class GradeBookTest {
 
         book.addAttestation(2, "Introduction to algebra and analysis", 5, "Vaskevich");
         book.addAttestation(2, "Introduction to discrete mathematics", 5, "Vlasov");
-        book.addAttestation(2, "Declarative programming", 5, "Vlasov");
+        book.addAttestation(2, "Declarative programming", 4, "Vlasov");
         book.addAttestation(2, "Imperative programming", 5, "Nesterenko");
         book.addAttestation(2, "English", 5, "Khotskina");
         book.setQualifyingWork("The best work", 5, "The best teacher");
@@ -70,7 +70,7 @@ class GradeBookTest {
         double expected = 49.0 / 10.0;
         double actual = book.getAverageScore();
         assertEquals(expected, actual);
-        assertTrue(book.hasHighScholarship());
+        assertFalse(book.hasHighScholarship());
         assertTrue(book.hasHonorsDegree());
     }
 
