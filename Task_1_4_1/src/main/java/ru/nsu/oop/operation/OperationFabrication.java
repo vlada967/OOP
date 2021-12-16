74ce59ccbc6d7781c6efdea6c14fdf2cb1e7e04a
Vlada Arkhipova <v.arkhipova1@g.nsu.ru>
2021-12-16 22:32:06 +0700

    lab4_1

--- Task_1_4_1/src/main/java/ru/nsu/oop/operation/OperationFabrication.java
+++ Task_1_4_1/src/main/java/ru/nsu/oop/operation/OperationFabrication.java
@@ -3,19 +3,33 @@ package ru.nsu.oop.operation;
 import ru.nsu.oop.operation.operations.*;
 
 public class OperationFabrication {
-    public static Operation getOperation(String operator) {
-        return switch (operator) {
-            case "log" -> new Logarithm();
-            case "sqrt" ->  new SquareRoot();
-            case "sin" -> new Sinus();
-            case "cos" -> new Cosine();
-            case "deg" -> new Degrees();
-            case "pow" -> new Power();
-            case "+" -> new Addition();
-            case "-" -> new Subtraction();
-            case "*" -> new Multiplication();
-            case "/" -> new Division();
-            default -> null;
+
+    private static Map<String, Operation> operations;
+
+    public OperationFabrication() {
+        operations = new HashMap<>() {
+            put("sqrt", new SquareRoot());
+            put("log", new Logarithm());
+            put("sin", new Sinus());
+            put("cos", new Cosine());
+            put("pow", new Power());
+            put("+", new Addition());
+            put("-", new Subtraction());
+            put("*", new Multiplication());
+            put("/", new Division());
+            put("deg", new Degrees());
         };
+    }
+
+    public static Operation getOperation(String operationName) {
+        Operation operation = operations.get(operationName);
+        if (operation == null) {
+            throw new IllegalArgumentException("The operation does not exist");
+        }
+        return operation;
+    }
+
+    public void addOperations(Map<String, Operation> extraOperations) {
+        operations.putAll(extraOperations);
     }
 }
